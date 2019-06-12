70afc63c1f7bf8edf619a5f5542795e6e6dcb16a
Subham Sarangi <subhamsarangi2016@gmail.com>
2019-06-12 21:10:38 +0530

    first commit: poject til, app things. and the url conf

---
+++ til/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import  include, url
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', include('things.urls', namespace="things")),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^logout/', LogoutView.as_view(), name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
